f76df6fb1d85fd6ba078a19c8a77538afbaf4f6c
Tate Mac <tateatevans2@gmail.com>
2025-02-28 12:47:44 +0200

    updated base

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/elevate-dev-site/',
+  base: '/site/',
   server: {
     host: "::",
     port: 8080,
